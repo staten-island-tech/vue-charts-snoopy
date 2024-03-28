7ed1908c3ea9941ddc005c9c924bf4a5a82ac5b0
maggiey8 <144460257+maggiey8@users.noreply.github.com>
2024-03-28 10:12:35 -0400

    wip drop down

--- snoopy/src/views/CardView.vue
+++ snoopy/src/views/CardView.vue
@@ -1,4 +1,14 @@
 <template>
+  <select @change="FilterData(value)">
+    <option value="0">All</option>
+    <option value="1">District 1</option>
+    <option value="2">District 2</option>
+    <option value="3">District 3</option>
+    <option value="4">District 4</option>
+    <option value="5">District 5</option>
+    <option value="6">District 6</option>
+    <option value="7">District 7</option>
+  </select>
   <h1>New York City Districts Information</h1>
   <div class="card">
     <DistrictCard 
@@ -14,15 +24,22 @@ import { ref, onMounted } from 'vue'
 import DistrictCard from '../components/DistrictCard.vue'
 
 const data = ref('')
+const Data = ref('')
+
 async function getDistrict() {
   let res = await fetch('https://data.cityofnewyork.us/resource/7z8d-msnt.json')
-  data.value = await res.json()
+  Data.value = await res.json()
+  data.value = Data
 }
 onMounted(() => {
   getDistrict()
 })
 
 console.log(data)
+
+function FilterData(num) {
+  data.value = Data.filter((district) => district===(num-1))
+}
 
 </script>
 
