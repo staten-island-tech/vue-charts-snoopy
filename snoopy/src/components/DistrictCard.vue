2316f3be10dae725d9c17d1361a01e203cf872c4
maggiey8 <144460257+maggiey8@users.noreply.github.com>
2024-03-28 09:45:56 -0400

    fixed data on load

--- snoopy/src/components/DistrictCard.vue
+++ snoopy/src/components/DistrictCard.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <h2>{{ District.district }}</h2>
-        <h3>{{ District.ytd_attendance_avg_ }}</h3>
-        <h3>{{ District.ytd_enrollemnt_avg_ }}</h3>
+        <h3>Attendance Average: {{ District.ytd_attendance_avg_ }}</h3>
+        <h3>Enrollemnt Average: {{ District.ytd_enrollment_avg_ }}</h3>
     </div>
 </template>
 
--- snoopy/src/views/CardView.vue
+++ snoopy/src/views/CardView.vue
@@ -1,53 +1,28 @@
 <template>
-  <h1>New York City Districts 1 to 35 Information</h1>
-  <div id="chart"></div>
+  <h1>New York City Districts Information</h1>
   <div class="card">
     <DistrictCard 
-    v-for="district in district" 
-    :key = "district"
-    :ytd_attendance_avg_="district.ytd_attendance_avg_"
-    :ytd_enrollment_avg_="district.ytd_enrollment_avg_"
+    v-for="district in data" 
+    :District = "district"
     />
   </div>
 </template>
 
 <script setup>
-// import DistrictCard from '@/components/DistrictCard.vue';
-// const URL = 'https://data.cityofnewyork.us/resource/7z8d-msnt.json'
-// let data = ''
-// import { createApp } from 'vue'
-// import App from './DoughnutView.vue';
-
-// // function onBeforeMount(callback: () => void): void
-
-// async function mounted() {
-//   try {
-//     let response = await fetch(URL)
-//     data = await response.json()
-//     console.log(data)
-//   } catch (e) {
-//     console.error(e)
-//   }
-// }
-// mounted()
-
-// createApp(App).mount('#chart')
-
 
 import { ref, onMounted } from 'vue'
 import DistrictCard from '../components/DistrictCard.vue'
 
-const district = ref('')
+const data = ref('')
 async function getDistrict() {
   let res = await fetch('https://data.cityofnewyork.us/resource/7z8d-msnt.json')
-  let data = await res.json()
-  district.value = data
+  data.value = await res.json()
 }
 onMounted(() => {
   getDistrict()
 })
 
-
+console.log(data)
 
 </script>
 
