66dfa8cf6a8e7f84c8494f417baa51be663262e9
maggiey8 <yangmaggie2006@gmail.com>
2024-03-31 22:22:16 -0400

    doughnut chart info

--- snoopy/src/views/DoughnutView.vue
+++ snoopy/src/views/DoughnutView.vue
@@ -36,20 +36,38 @@ export default {
     return {
       loaded: false,
       chartData: {
-        datasets: [{
-          data: chartData
-        }]
+        labels: [
+          'District 1', 'District 2', 'District 3', 'District 4', 'District 5',
+          'District 6', 'District 7', 'District 8', 'District 9', 'District 10',
+          'District 11', 'District 12', 'District 13', 'District 14', 'District 15',
+          'District 16', 'District 17', 'District 18', 'District 19', 'District 20',
+          'District 21', 'District 22', 'District 23', 'District 24', 'District 25',
+          'District 26', 'District 27', 'District 29', 'District 30', 'District 30',
+          'District 31', 'District 32',
+        ],
+        datasets: [{ data: [] }]
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: true,
+        backgroundColor: ['#caf0f8', '#ADD8f9']
       }
     }
   },
-  async mounted() {
-  try {
-    let response = await fetch(URL)
-    chartData = await response.json()
-    console.log(data)
-    this.loaded = true
-  } catch (e) {
-    console.error(e)
+async mounted() {
+    try {
+      const res = await fetch('https://data.cityofnewyork.us/resource/7z8d-msnt.json')
+      let data = await res.json()
+      
+      for (let i=0; i<32; i++) {
+        this.chartData.datasets[0].data.push(data[i].ytd_enrollment_avg_)
+      }
+
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
+    }
+    console.log(this.chartData)
   }
 }
 }
